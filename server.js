3a737ad7d75d4cdeaaede0e1bd3d188f3b4d28ab
bjugoy <richardtlibres@gmail.com>
2023-06-18 23:02:22 +0200

    v.5 login register and logout endpoints

--- server.js
+++ server.js
@@ -3,16 +3,21 @@ const favorites = require("./recipe-favorites")
 const app = express();
 const path = require("path");
 const http = require("http");
+const bcrypt = require('bcrypt'); // password encryptionn
+const cookieParser = require('cookie-parser')
 
 const axios = require('axios');
 const key_api = 'ff9c2c48de514451bba22bb3017484c5'
 const key_api2 = 'b9626a5b5716476da85bed6e7fba5387'
 
 
+
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "src")));
+app.use(cookieParser());
+
 
 
 app.get('/', (req, res) => {
@@ -82,6 +87,97 @@ app.put("/updateRecipeInfo", async (req, res) => {
 
   res.sendStatus(200)
 })
+
+// --- SESSION MANAGEMENT ---
+const sessions = [];
+const users = [];
+let nextUserId = 1;
+let sessionCount = 1;
+
+app.get("/users", async (req, res) =>{
+  res.json(users);
+})
+
+app.get("/sessions", async (req,res) =>{
+  res.json(sessions);
+})
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body; //eingegebenes username und password
+
+  try {
+    const user = users.find((user) => user.username === username); //vergleicht usernames im user array
+
+    if (!user) {
+      return res.status(401).send("Invalid username or password");
+    }
+    const passwordMatch = await bcrypt.compare(password, user.hashedPassword);
+    if (!passwordMatch) {
+      return res.status(401).send("Invalid username or password");
+    }
+
+
+    const existingSession = sessions.find((session) => session.userId === user.userId);
+    if (existingSession) {
+      res.cookie("session", existingSession.sessionId);
+    } else {
+      const sessionId = sessionCount + 1000;
+      sessionCount++;
+
+      const session = {
+        sessionId,
+        userId: user.userId,
+      };
+      sessions.push(session);
+
+      res.cookie("session", sessionId);
+    }
+
+    res.send("Login successful");
+  } catch (error) {
+    res.status(500).send("Internal server error");
+  }
+})
+
+app.post("/register", async (req, res) => {
+  const {username, password} = req.body;
+  const userId = nextUserId++;
+
+try{
+  const hashedPassword = await bcrypt.hash(password, 5)
+  const newUser = {
+    userId,
+    username,
+    hashedPassword,
+  }
+  users.push(newUser)
+  res.send(newUser)
+} catch{
+  res.status(500)
+}
+})
+
+app.post("/logout", (req, res) => {
+  const sessionId = req.cookies.session;
+
+  if (!sessionId) {
+    return res.status(401).send("Unauthorized");
+  }
+
+  //sessions array nach der session suchen mit der erhaltenen sessionId
+  const sessionIndex = sessions.findIndex((session) => session.sessionId === sessionId);
+
+  //wenn gefunden, löschen(von array)
+  if (sessionIndex !== -1) {
+    sessions.splice(sessionIndex, 1);
+  }
+
+  //cookie clearen
+  res.cookie("session", "", { expires: new Date(0) });
+
+  res.send("Logged out successfully");
+});
+
 
 
 app.listen(3001, () => {
